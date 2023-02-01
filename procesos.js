d4654325ebffda7d27afe03e375b3feb52a07d5b
julianandrada <aaajulianggg@gmail.com>
2023-01-31 21:46:48 -0300

    subiendo archivo

---
+++ procesos.js
@@ -0,0 +1,14 @@
+/* datos del usuario **/
+document.getElementById('button').onclick = function () { 
+    document.getElementById("datos1").innerHTML = " email: courtney.weaver@ejemplo.com";
+    document.getElementById("datos2").innerHTML = " fecha de nacimiento : 01/06/1999";
+    document.getElementById("datos3").innerHTML = " telefono : (573) 543-0095";
+}
+document.getElementById('especializaciones').onclick = function () {
+    document.getElementById("e1").innerHTML = "Diseño de producto digital"
+    document.getElementById("e2").innerHTML = "Arquitectura de informacion"
+    document.getElementById("e3").innerHTML = "fotografia y estilos de vida"
+    document.getElementById("e4").innerHTML = "edicion de video y fotos"
+    document.getElementById("e5").innerHTML = "investigacion y experiencia del usuario"
+    
+}
